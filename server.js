e7458231f6d7705cf3097f92004673ed0fc4911e
mujahirabbasi <mujahirabbasi@gmail.com>
2016-09-23 00:45:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/mujahir.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'mujahir.jpg'));
 });
 app.get('/article-one',function(req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
