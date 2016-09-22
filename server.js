cf8a491b69a4c2bb277723befdd792568804912f
mujahirabbasi <mujahirabbasi@gmail.com>
2016-09-22 22:21:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/article-two',function(req,res){
 res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+res.sendFile(path.join(__dirname, 'ui', 'artcile-three.html'));
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
