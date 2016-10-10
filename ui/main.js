701470ae884c95405f83938ff2ba9407bc4c4308
mujahirabbasi <mujahirabbasi@gmail.com>
2016-10-10 13:50:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,14 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='This is SULTAN MIRZA';
+
+var img=document.getElementById('muji');
+var marginLeft=0;
+ function moveRight(){
+     marginLeft=marginLeft+1;
+     img.style.marginLeft=margingLeft+'px';
+ }
+ img.onclick=function(){
+     var interval=setInterval(moveRight,50);
+ };
+ 
