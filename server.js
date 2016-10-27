33c4fb629ef71e72f716f664e489f7f224debf16
mujahirabbasi <mujahirabbasi@gmail.com>
2016-10-27 16:58:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,9 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/test-db',function(req,res){
+    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
