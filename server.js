ec24913b8bf55482eb0cc37ed8cc916cdf534c39
mujahirabbasi <mujahirabbasi@gmail.com>
2016-11-19 00:00:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var articles={
  <h1>ABOUT</h1>
  
  <div >
- <img  src="https://pbs.twimg.com/profile_images/799658699195224064/8i2lKjhm.jpg" class="img-medium1"  />
+ <img  src="https://pbs.twimg.com/profile_images/799658699195224064/8i2lKjhm.jpg" class="img-medium1" height="42" width="42" />
  </div>
     </p>`
     },
